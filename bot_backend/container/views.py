566960e26c74acc70c869a89d07c8ff6a630eb91
bakyt <bakytniazaliev290304@gmail.com>
2022-12-11 16:16:09 +0700

    as;ldkfj

--- bot_backend/container/views.py
+++ bot_backend/container/views.py
@@ -7,7 +7,7 @@ from rest_framework.parsers import JSONParser
 from rest_framework.generics import ListAPIView, UpdateAPIView, ListCreateAPIView
 from rest_framework.exceptions import ValidationError
 
-from .serializers import ContainerSerializer
+from .serializers import ContainerSerializer, ReportSerializer
 from .models import Container
 
 from users.models import CustomerProfile, Session, User
@@ -24,14 +24,18 @@ class UpdateContainer(UpdateAPIView):
     serializer_class = ContainerSerializer
 
 
-# @require_POST
-# def report_post(request):
-#     data = JSONParser().parse(request)
-#     container = get_object_or_404(Container, id=data['container'])
-#     is_active = True
-#     for value in data.values()[1:]:
-#         if value == False:
-#             is_active = False
+@require_POST
+def report_post(request):
+    data = JSONParser().parse(request)
+    is_active = True
+    for value in data.values()[1:]:
+        if value == False:
+            is_active = False
+            break
+    container = Container.objects.filter(pk=data['container']).update(is_active=is_active)
+    serializer = ReportSerializer(data=data).save()
+    return JsonResponse(serializer.data)
+    
     
 
 
@@ -39,7 +43,9 @@ class UpdateContainer(UpdateAPIView):
 @csrf_exempt
 def session_post(request):
     data = JSONParser().parse(request)
-    user = get_object_or_404(User, username=data['customer'])
+    user = get_object_or_404(User, rfid=data['rfid'])
+    if user in User.objects.fiter(role="Customer"):
+        return 
     data['customer'] = CustomerProfile.objects.get(user=user)
     object = Session(customer=data['customer'], points=data['points'])
     serializer = SessionSerializer(data=data)
